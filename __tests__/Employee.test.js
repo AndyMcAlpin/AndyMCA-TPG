ed7f3cf038dea68baad74b2ca8671cc6c6f4b4cb
Andy McAlpin <AndrewMcAlpin@gmail.com>
2022-03-03 00:01:35 -0600

    employee object mostly finished

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,16 +1,30 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates an employee object', () => {
-    const employee = new Employee('nombre', '123@abc.com');
+    const employee = new Employee('nombre', '123@abc.com', '3');
 
     expect(employee.name).toBe('nombre');
-    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.id).toBe('3');
     expect(employee.email).toBe('123@abc.com');
 
 });
 
 test('gets the employees name', () => {
-    const employee = new Employee('nombre', '123@abc.com');
+    const employee = new Employee('nombre', '123@abc.com', '3');
 
     expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
 })
+
+test('gets the employees id', () => {
+    const employee = new Employee('nombre', '123@abc.com', '3');
+
+    expect(employee.getID()).toEqual(expect.stringContaining(employee.id.toString()));
+})
+
+test('gets the employees email', () => {
+    const employee = new Employee('nombre', '123@abc.com', '3');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+})
+
+
